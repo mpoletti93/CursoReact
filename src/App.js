a4404d24797e28679f9fa06e2cca942caf861497
mariano <mariano.poletti93@gmail.com>
2022-08-22 22:36:44 -0300

    archivo nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Footer from './Componentes/Footer';
+import Header from './Componentes/Header';
 
 function App() {
   return (
   <div>
+    <Header />
       <h1>HOLA MUNDO</h1>
-      <h2> Esto es un H2</h2>
+      <h2> Esto es un H2 01</h2>
+    <Footer />
   </div>
   );
 }
