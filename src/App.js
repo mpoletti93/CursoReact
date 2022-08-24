b92f2087ff89f1db3e47a9755043932ce74a887c
mariano <mariano.poletti93@gmail.com>
2022-08-24 19:30:31 -0300

    agregado de navvar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Footer from './Componentes/Footer';
 import Header from './Componentes/Header';
+import Navvar from './Componentes/NavBar';
 
 function App() {
   return (
-  <div>
+  <div className='container-fluid'>
     <Header />
+    <Navvar />
       <h1>HOLA MUNDO</h1>
       <h2> Esto es un H2 01</h2>
     <Footer />
--- src/Componentes/NavBar.jsx
+++ src/Componentes/NavBar.jsx
@@ -1,1 +1,24 @@
 import React from "react";
+import Logo from "../Imagenes/Logo.jpg"
+
+const Navvar = () => {
+    return (
+<div className="container">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#"><img src={Logo} width="150" alt="Guapasbb"/></a>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">Home </a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Productos</a>
+                    </li>
+                </ul>
+            </div>
+    </nav>
+</div>
+    )
+}
+
+export default Navvar;
