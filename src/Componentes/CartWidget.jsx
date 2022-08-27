39479d606117ca9d2d96c3e90162ff0d11599fb2
mariano <mariano.poletti93@gmail.com>
2022-08-26 21:33:08 -0300

    Segundo desafio

---
+++ src/Componentes/CartWidget.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Cart from "../Imagenes/cart.png"
+
+const CartWidget = () => {
+    return (
+<img  className="nav-item" src={Cart} width="100" alt="Cart" />
+    )
+}
+
+export default CartWidget;
--- src/Componentes/NavBar.jsx
+++ src/Componentes/NavBar.jsx
@@ -1,21 +1,25 @@
 import React from "react";
 import Logo from "../Imagenes/Logo.jpg"
+import CartWidget from "./CartWidget"
+import ItemList from "./ItemListContainer"
 
 const Navvar = () => {
     return (
 <div className="container">
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#"><img src={Logo} width="150" alt="Guapasbb"/></a>
+        <a className="navbar-brand"><img src={Logo} width="150" alt="Guapasbb"/></a>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item active">
-                        <a className="nav-link" href="#">Home </a>
+                        <a className="nav-link" href="">Home </a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Productos</a>
+                        <a className="nav-link" href="">Productos</a>
                     </li>
                 </ul>
             </div>
+                <CartWidget />
+                <ItemList />
     </nav>
 </div>
     )
