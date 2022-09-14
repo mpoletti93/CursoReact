41b8e009c6543084c475809478b68452ee199932
mariano <mariano.poletti93@gmail.com>
2022-09-13 21:41:44 -0300

    +arreglos

--- src/Componentes/ItemDetail.jsx
+++ src/Componentes/ItemDetail.jsx
@@ -8,12 +8,6 @@ const ItemDetail = ({ item }) => {
             <img src={item.img} alt={item.title} />
             <div className="infoDetail">
                 <h2>{item.title}</h2>
-                <p>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                    Reiciendis ratione aliquam ducimus debitis quibusdam? Nulla
-                    molestiae voluptates, ex sapiente, non porro omnis provident
-                    a hic facere, ab deleniti commodi aut!
-                </p>
                 <h3>${item.price}.-</h3>
 
                 <ItemCount stock={10} initial={1} />
