4020ffe4ab566357cfc796973f156a4854ae08c2
mariano <mariano.poletti93@gmail.com>
2022-09-28 00:18:31 -0300

    segunda entrega final

--- src/Componentes/CartContainer.jsx
+++ src/Componentes/CartContainer.jsx
@@ -1,4 +1,5 @@
 import CartItem from "./CartItem";
+import CartForm from "./CartForm";
 import { cartContext } from "./CartContext";
 import { useContext } from 'react';
 
@@ -7,12 +8,17 @@ const CartContainer = (props) => {
     const { deleteFromCart } = useCartContext;
     
     return (
+        <div>
             <div className="dropShadow">
                 <h2>Detalles productos</h2>
                 {props.carrito.map(item => (
                     <CartItem key={item.product.id} item={item} deleteFromCart={deleteFromCart} />
                 ))}
             </div>
+            <div id="containerForm" className="dropShadow">
+                <CartForm carrito={props.carrito} />
+            </div>
+        </div>
     )
 }
 export default CartContainer
